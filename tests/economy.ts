e876ca72d5d50e2e6416a5677fff29d18fdb0050
Vinyarion <38413862+VinyarionHyarmendacil@users.noreply.github.com>
2024-11-16 21:08:51 -0600

    Update economy.ts

--- tests/economy.ts
+++ tests/economy.ts
@@ -1,6 +1,9 @@
 import { failUnauthenticated, test, testOperation } from "./_utilities.js";
 import { state } from "./_cache.js";
 
+const blackCatProductId = "prod_c9d1cf9b-e3be-4bed-8386-49f0a8d32910";
+const blackCatOwnerId = "usr_3d10ca69-6586-40a3-aa1b-a0c9e38a0d20";
+
 test.before(failUnauthenticated);
 
 test.serial.failing(
@@ -53,6 +56,43 @@ test(testOperation, "getSubscriptions", {
 test(testOperation, "getLicenseGroup", {
 	parameters: {
 		licenseGroupId: "lgrp_608513da-b213-4e15-80af-bd88c27f0979"
+	},
+	statusCode: 200
+});
+
+test(testOperation, "getProductListing", {
+	parameters: {
+		productId: blackCatListingId
+	},
+	statusCode: 200
+});
+
+test(testOperation, "getProductListings", {
+	parameters: {
+		userId: blackCatOwnerId,
+        hydrate: true
+	},
+	statusCode: 200
+});
+
+test(testOperation, "getTokenBundles", {
+	statusCode: 200
+});
+
+test(testOperation, "getTiliaStatus", {
+	statusCode: 200
+});
+
+test(testOperation, "getTiliaTos", {
+	parameters: {
+		userId: state.get("current-user").id
+	},
+	statusCode: 200
+});
+
+test(testOperation, "getBalance", {
+	parameters: {
+		userId: state.get("current-user").id
 	},
 	statusCode: 200
 });
